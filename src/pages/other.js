f6db4124c65ec7328e8b4b47fe02cc2d19a58539
Gunnar Ingi Stefansson <gis20@hi.is>
2020-03-10 14:52:44 +0000

    image carousel

--- src/pages/other.js
+++ src/pages/other.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useRef, useEffect, useState } from "react"
 import Layout from "../components/layout"
 import { LinkExt } from "../components/Link"
 import styled from "styled-components"
@@ -27,7 +27,8 @@ const Slider = styled.div`
   align-self: center;
 `
 
-const ImageWrapper = styled.div`
+const SlideList = styled.ul`
+  list-style: none;
   display: flex;
   overflow-x: auto;
   scroll-snap-type: x mandatory;
@@ -35,6 +36,7 @@ const ImageWrapper = styled.div`
   scroll-behavior: smooth;
   -webkit-overflow-scrolling: touch;
 
+  width: 100%;
   &::-webkit-scrollbar {
     height: 1rem;
   }
@@ -44,61 +46,172 @@ const ImageWrapper = styled.div`
   &::-webkit-scrollbar-track {
     background: ${props => props.theme.highlight};
   }
+`
+
+const SlideContainer = styled.li`
+  min-width: 100%;
+  min-height: 600px;
+  margin: 0;
+  scroll-snap-align: start;
+
+  position: relative;
+
+  &:first-child {
+    margin-left: 0;
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
 
   & > img {
-    min-width: 100%;
-    max-height: 600px;
-    margin: 0 1rem;
-    scroll-snap-align: start;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
+  }
 
-    &:first-child {
-      margin-left: 0;
-    }
+  & > img:focus {
+    border: 1px solid red;
+  }
 
-    &:last-child {
-      margin-right: 0;
-    }
+  & > .next {
+    position: absolute;
+    top: 45%;
+    bottom: 45%;
+    right: 1px;
+    background: black;
+  }
+
+  & > .prev {
+    position: absolute;
+    top: 45%;
+    bottom: 45%;
+    left: 0rem;
+    background: black;
   }
 `
 
-const Carousel = ({ images }) => {
-  const [position, setPosition] = useState(0)
+const Slide = ({ image, scroll, scroll2 }) => {
+  const slideRef = useRef(null)
 
-  const sorted = images.sort((a, b) => a.id > b.id)
+  const next = () => {
+    if (slideRef && slideRef.current) {
+      console.log(slideRef.current.offsetWidth)
+      scroll(slideRef.current.offsetWidth)
+    }
+  }
+
+  const prev = () => {
+    if (slideRef && slideRef.current) {
+      console.log(slideRef.current.offsetWidth)
+      scroll(-slideRef.current.offsetWidth)
+    }
+  }
+
+  const keyhandler = e => {
+    if (e.key === "ArrowLeft") {
+      prev()
+    }
+    if (e.key === "ArrowRight") {
+      next()
+    }
+  }
+
+  const next2 = () => {
+    scroll2(slideRef.current)
+  }
+
+  return (
+    <SlideContainer
+      ref={slideRef}
+      id={image.id}
+      key={image.id}
+      onKeyUp={keyhandler}
+      id={image.id}
+    >
+      <img src={image.src} alt={image.description} />
+      <button className="prev" onClick={prev}>
+        prev
+      </button>
+      <button className="next" onClick={next}>
+        next
+      </button>
+    </SlideContainer>
+  )
+}
+
+const Carousel = ({ images }) => {
+  // want to move the scrolling functionality into the carousel,
+  // only have the slide provide a ref to it as an argument
+  const [position, setPosition] = useState(0)
+  const [slide, setSlide] = useState(null)
+
+  const carouselReducer = (state, action) => {
+    switch (action.type) {
+      case "NEXT":
+        if (state.position === images.length - 1)
+          return { ...state, position: 0 }
+        else return { ...state, position: state.position + 1 }
+      case "PREV":
+        if (state.position === 0)
+          return { ...state, position: images.length - 1 }
+        else return { ...state, position: state.position - 1 }
+    }
+  }
+
+  const [state, dispatch] = useReducer(carouselReducer, {
+    position: 0,
+  })
+
+  useEffect(() => {
+    const img = document.getElementById(images[position].id)
+    img.focus()
+  }, [position, images])
+
+  useEffect(() => {}, [position])
+
+  const sliderRef = useRef(null)
+  const scroll = offset => {
+    const { current: slider } = sliderRef
+    if (slider) {
+      const { scrollWidth, scrollLeft } = slider
+      const isLeftEdge = position === 0
+      const isRightEdge = position === images.length - 1
+
+      if (isLeftEdge) {
+      }
+
+      if (scrollLeft + offset < 0)
+        slider.scrollTo({ left: scrollWidth, behavior: "smooth" })
+      // don't know why scrollwidth is 1px bigger then expected..
+      if (scrollLeft + offset >= scrollWidth - 1)
+        slider.scrollTo({ left: 0, behavior: "smooth" })
+      else slider.scrollTo({ left: scrollLeft + offset, behavior: "smooth" })
+
+      setPosition(p => {
+        const newPos = offset > 0 ? p + 1 : p - 1
+        if (newPos < 0) return images.length - 1
+        if (newPos > images.length - 1) return 0
+        return newPos
+      })
+    }
+  }
 
   return (
     <Slider>
-      {sorted[position - 1] ? (
-        <a
-          href={`#${sorted[position - 1].id}`}
-          onClick={() => setPosition(p => p - 1)}
-        >
-          prev
-        </a>
-      ) : (
-        <p>prev</p>
-      )}
-      <ImageWrapper>
-        {sorted.map(image => (
-          <img
-            id={image.id}
+      <SlideList ref={sliderRef}>
+        {images.map(image => (
+          <Slide
             key={image.id}
-            src={image.src}
-            alt={image.description}
+            image={image}
+            scroll={scroll}
+            scroll2={setSlide}
           />
         ))}
-      </ImageWrapper>
-      {sorted[position + 1] ? (
-        <a
-          href={`#${sorted[position + 1].id}`}
-          onClick={() => setPosition(p => p + 1)}
-        >
-          next
-        </a>
-      ) : (
-        <p>next</p>
-      )}
+      </SlideList>
     </Slider>
   )
 }
